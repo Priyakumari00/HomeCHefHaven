e13b1963838c27a8229fdc0c896af0f28d2ca13f
Priyakumari00 <priya.k.p13579@gmail.com>
2023-10-20 14:03:48 +0530

    first commit

---
+++ src/utils/socials.js
@@ -0,0 +1,36 @@
+import { GitHub, Linkedin, Mail, Phone, Twitter } from "react-feather";
+
+const socials = [
+	// {
+	// 	username: "@akshatmittal61",
+	// 	link: "https://twitter.com/akshatmittal61",
+	// 	service: "twitter",
+	// 	icon: <Twitter />,
+	// },
+	{
+		username: "@PriyaKumari",
+		link: "https://www.linkedin.com/in/priya-kumari-9a7a06242",
+		service: "linkedin",
+		icon: <Linkedin />,
+	},
+	{
+		username: "@PriyaKumari00",
+		link: "https://www.github.com/PriyaKumari00",
+		service: "github",
+		icon: <GitHub />,
+	},
+	{
+		username: "+91 93663 13485",
+		link: "tel:919366313485",
+		service: "phone",
+		icon: <Phone />,
+	},
+	{
+		username: "priya.k.p.13579@gmail.com",
+		link: "mailto:priya.k.p.13579@gmail.com",
+		service: "mail",
+		icon: <Mail />,
+	},
+];
+
+export default socials;
